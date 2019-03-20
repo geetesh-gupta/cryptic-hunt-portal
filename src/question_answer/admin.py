b553f6eff56b4d8dffbeb82c7171877bcbce32f1
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-03-20 12:35:44 +0530

    Update QuestionAdmin list

--- src/question_answer/admin.py
+++ src/question_answer/admin.py
@@ -5,6 +5,8 @@ from .models import Question, Answer # Import Question and Answer modules
 # Register Question module on the admin interface
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question', 'quiz_category', 'order']
+    list_filter = ['quiz_category__name', 'order']
     class Meta:
         model = Question
         # All fields of Question
