254c4cf5f0d02bd2d45997dd5f3e46b25c08cb7c
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-03-20 15:10:43 +0530

    Add comments in admin.py

--- src/question_answer/admin.py
+++ src/question_answer/admin.py
@@ -5,7 +5,9 @@ from .models import Question, Answer  # Import Question and Answer modules
 # Register Question module on the admin interface
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
+    # Display customised list on admin page
     list_display = ['Questions', 'Quizzes', 'order']
+    # Add filters on admin page
     list_filter = ['quiz__name', 'order']
 
     class Meta:
@@ -13,11 +15,13 @@ class QuestionAdmin(admin.ModelAdmin):
         # All fields of Question
         fields = '__all__'
 
+    # list_display for quizzes as comma-separated-list
     def Quizzes(self, obj):
         return ", ".join([
             quiz.name for quiz in obj.quiz.all()
         ])
 
+    # list_display for questions
     def Questions(self, obj):
         return obj.question[3:-4]
 
@@ -25,7 +29,9 @@ class QuestionAdmin(admin.ModelAdmin):
 # Register Answer module on the admin interface
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
+    # Display customised list on admin page
     list_display = ['Answers', 'Questions', 'Question_Orders']
+    # Add filters on admin page
     list_filter = ['questions__quiz__name', 'questions__order']
 
     class Meta:
@@ -33,14 +39,17 @@ class AnswerAdmin(admin.ModelAdmin):
         # All fields of Answer
         fields = '__all__'
 
+    # list_display for answers
     def Answers(self, obj):
         return obj.name
 
+    # list_display for questions as comma-separated-list
     def Questions(self, obj):
         return ", ".join([
-            que.slug for que in obj.questions.all()
+            que.slug[:20] for que in obj.questions.all()
         ])
 
+    # list_display for question_orders as comma-separated-list
     def Question_Orders(self, obj):
         return ", ".join([
             str(que.order) for que in obj.questions.all()
--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -5,6 +5,7 @@ from .models import Quiz  # Import Quiz module
 # Register Quiz module on the admin interface
 @admin.register(Quiz)
 class QuizAdmin(admin.ModelAdmin):
+    # Display customised list on admin page
     list_display = ['name', 'published']
 
     class Meta:
