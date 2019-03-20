1e942f68ba818fd832ced3820f5e97714158e32a
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-03-20 12:01:06 +0530

    Add Answer Model

--- src/question_answer/admin.py
+++ src/question_answer/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from .models import Answer # Import Question and Answer modules
+
+
+# Register Answer module on the admin interface
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Answer
+        # All fields of Answer
+        fields = '__all__'
--- src/question_answer/models.py
+++ src/question_answer/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+
+class Answer(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
