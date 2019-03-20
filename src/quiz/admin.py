ae9bdf9c2c287f7e147f2cd301bdcb06c08d876b
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-03-20 11:59:11 +0530

    Add QuizCategory Model

--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from .models import QuizCategory # Import QuizCategory module
+
+
+# Register QuizCategory module on the admin interface
+@admin.register(QuizCategory)
+class QuizCategoryAdmin(admin.ModelAdmin):
+    class Meta:
+        model = QuizCategory
+        # All fields of QuizCategory
+        fields = '__all__'
--- src/quiz/models.py
+++ src/quiz/models.py
@@ -1,1 +1,22 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.utils.text import slugify
+
+
+class QuizCategory(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField()
+    about = RichTextUploadingField(null=True, blank=True, default='')
+    date_published = models.DateTimeField()
+    published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['date_published']
+        verbose_name_plural = 'quiz categories'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(QuizCategory, self).save(*args, **kwargs)
