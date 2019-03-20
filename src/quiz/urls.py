405971943ce278337748c18bc85808b5a20fc6e9
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-03-20 22:34:10 +0530

    Create QuizView

--- src/quiz/urls.py
+++ src/quiz/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from quiz.views import IndexView
+from quiz.views import IndexView, QuizView
 
 app_name = 'quiz'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('<slug:slug>/', QuizView.as_view(), name='quiz')
 ]
--- src/quiz/views.py
+++ src/quiz/views.py
@@ -11,3 +11,12 @@ class IndexView(TemplateView):
         context['quizzes'] = quizzes
         return context
 
+
+class QuizView(TemplateView):
+    template_name = 'quiz/quiz.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(QuizView, self).get_context_data(**kwargs)
+        quiz = Quiz.objects.filter(published=True, slug=kwargs['slug']).all()[0]
+        context['quiz'] = quiz
+        return context
