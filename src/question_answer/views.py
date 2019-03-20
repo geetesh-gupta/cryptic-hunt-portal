449e558cddeeaa6eeda15630e5ff510ec27d1366
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-03-20 12:02:33 +0530

    Create IndexView

--- src/question_answer/views.py
+++ src/question_answer/views.py
@@ -1,1 +1,12 @@
 from django.views.generic import TemplateView
+from question_answer.models import Question
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        question_list = Question.objects.filter(published=True)
+        context['question_list'] = question_list
+        return context
