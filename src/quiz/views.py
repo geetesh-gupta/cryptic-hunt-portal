6aacd8e4dd6b5befa385d35db89fa708dedce5ae
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-03-21 23:33:57 +0530

    Inherit ListView in QuizIndexView

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -1,15 +1,11 @@
-from django.views.generic import TemplateView
 from quiz.models import Quiz
+from django.views.generic import TemplateView, ListView
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
+    model = Quiz
     template_name = 'quiz/index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
-        quizzes = Quiz.objects.filter(published=True)
-        context['quizzes'] = quizzes
-        return context
+    context_object_name = 'quizzes'
 
 
 class QuizView(TemplateView):
