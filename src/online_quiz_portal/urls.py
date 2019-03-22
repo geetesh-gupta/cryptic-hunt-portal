2baf163f374e9dc0a57bcc63d38f06a2ae352efd
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-03-22 11:07:25 +0530

    Transfer IndexView to main app

--- src/online_quiz_portal/urls.py
+++ src/online_quiz_portal/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from main.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index'),
+    path('', include('main.urls'), name='main'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('quiz/', include('quiz.urls'))
 ]
