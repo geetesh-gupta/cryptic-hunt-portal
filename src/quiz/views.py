eb78d99d94808d19710fd2f0bbdfd999c9b5d16d
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-03-22 08:45:39 +0530

    Refactor QuizView quiz queries

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -12,13 +12,12 @@ class IndexView(ListView):
 
 
 class QuizView(UpdateView):
-    # http_method_names = ['post']
     template_name = 'quiz/quiz.html'
     context_object_name = 'quiz'
     form_class = AnswerForm
 
     def form_valid(self, form):
-        num_of_que = Quiz.objects.all().filter(slug=self.kwargs['slug'])[0].questions.all().count()
+        num_of_que = self.get_quiz(self.kwargs['slug']).questions.count()
         if (self.kwargs['num'] < num_of_que):
             form.instance.current_question = self.kwargs['num'] + 1
         else:
@@ -26,20 +25,20 @@ class QuizView(UpdateView):
         return super().form_valid(form)
 
     def get_queryset(self):
-        quiz = Quiz.objects.filter(published=True, slug=self.kwargs['slug']).all()
+        quiz = Quiz.objects.filter(published=True, slug=self.kwargs['slug'])
         return quiz
 
     def get_success_url(self):
-        num_of_que = Quiz.objects.all().filter(slug=self.kwargs['slug'])[0].questions.all().count()
+        num_of_que = self.get_quiz(self.kwargs['slug']).questions.count()
         if (self.kwargs['num'] < num_of_que):
             return reverse('quiz:quiz', kwargs={'slug': self.kwargs['slug'], 'num': self.kwargs['num'] + 1})
         else:
             return reverse('quiz:result', kwargs={'slug': self.kwargs['slug']})
 
     def get_context_data(self, **kwargs):
-        quiz = Quiz.objects.filter(published=True, slug=self.kwargs['slug']).all()[0]
+        quiz = self.get_quiz(self.kwargs['slug'])
         cur_que_num = self.kwargs['num']
-        cur_que = QuestionOrder.objects.all().filter(quiz=quiz, order=cur_que_num)[0].question
+        cur_que = QuestionOrder.objects.filter(quiz=quiz, order=cur_que_num)[0].question
         context = super(QuizView, self).get_context_data(**kwargs)
         context['current_question'] = cur_que
         return context
@@ -51,10 +50,14 @@ class QuizView(UpdateView):
         else:
             return super(QuizView, self).dispatch(request, *args, **kwargs)
 
+    def get_quiz(self, slug):
+        return Quiz.objects.filter(published=True, slug=slug)[0]
+
+
 class ResultView(DetailView):
     template_name = 'quiz/result.html'
     context_object_name = 'result'
 
     def get_queryset(self):
-        quiz = Quiz.objects.filter(published=True, slug=self.kwargs['slug']).all()
+        quiz = Quiz.objects.filter(published=True, slug=self.kwargs['slug'])
         return quiz
